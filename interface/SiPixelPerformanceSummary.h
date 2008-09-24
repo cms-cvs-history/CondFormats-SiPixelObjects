21c8c173ec7ccdfdfb26f0fd2b2ea07250e11b0e
schuang <sha1-65eb9735417a340a7fc69881308d83c349d3a9ce@cern.ch>
2008-09-24 08:53:37 +0000

    same as revision 1.2 but for V_21X_BRANCH

--- interface/SiPixelPerformanceSummary.h
+++ interface/SiPixelPerformanceSummary.h
@@ -8,22 +8,35 @@
 #include<boost/cstdint.hpp>
 
 
-#define kDetSummarySize 3 // number of values kept in DetSummary.performanceValues
+#define kDetSummarySize 108 // number of values kept in DetSummary.performanceValues
 #define kDefaultValue -99.9 
 
 
 class SiPixelPerformanceSummary {
 public:
+  /*
+  struct DetRegistry {
+    uint32_t detId;
+    uint32_t ibegin;
+    uint32_t iend;
+  };
+  class StrictWeakOrdering {
+  public:
+    bool operator() (const DetRegistry& p, const uint32_t& i) const { return p.detId < i; }
+  }; 
+  typedef std::vector<DetRegistry> Registry;
+  typedef Registry::const_iterator RegistryIterator;
+  */
   struct DetSummary { 
     uint32_t detId_;
     std::vector<float> performanceValues_;
   }; 
   class StrictWeakOrdering { // sort DetSummaries by detId
   public:
-    bool operator() (const DetSummary& detSumm, const uint32_t& otherDetId) const { 
-      return (detSumm.detId_ < otherDetId); 
+    bool operator() (const DetSummary& detSummary, const uint32_t& otherDetId) const { 
+      return (detSummary.detId_ < otherDetId); 
     };
-  };
+  }; 
   class MatchDetSummaryDetId : public std::unary_function<DetSummary, bool> { 
   public: 
     MatchDetSummaryDetId(const uint32_t& detId) : detId_(detId) {} 
@@ -37,21 +50,24 @@ public:
   SiPixelPerformanceSummary();
  ~SiPixelPerformanceSummary();
   
-  void clear() { allDetSummaries_.clear(); }; 
-  unsigned int size() { return allDetSummaries_.size(); };
+  void clear() { allDetSummaries_.clear(); }
+  unsigned int size() { return allDetSummaries_.size(); }
 
-  std::vector<DetSummary> getAllDetSummaries() const { return allDetSummaries_; };
+  std::vector<DetSummary> getAllDetSummaries() const { return allDetSummaries_; }
 
-          void setRunNumber(unsigned int runNumber) { runNumber_ = runNumber; }; 
-  unsigned int getRunNumber() const { return runNumber_; };
+          void setRunNumber(unsigned int runNumber) { runNumber_ = runNumber; }
+  unsigned int getRunNumber() const { return runNumber_; }
   
-                void setTimeValue(unsigned long long timeValue) { timeValue_ = timeValue; };
-  unsigned long long getTimeValue() const { return timeValue_; };
+          void setNumberOfEvents(unsigned int numberOfEvents) { numberOfEvents_ = numberOfEvents; }
+  unsigned int getNumberOfEvents() const { return numberOfEvents_; }
+  
+                void setTimeValue(unsigned long long timeValue) { timeValue_ = timeValue; }
+  unsigned long long getTimeValue() const { return timeValue_; }
     
   void print() const; 
   void print(const uint32_t detId) const; 
   void print(const std::vector<float>& performanceValues) const; 
-  void printall() const;  
+  void printAll() const;  
   
   std::pair<bool, std::vector<DetSummary>::iterator> initDet(const uint32_t detId); 
   std::pair<bool, std::vector<DetSummary>::iterator>  setDet(const uint32_t detId, 
@@ -59,8 +75,37 @@ public:
   void getAllDetIds(std::vector<uint32_t>& vDetIds) const;
   void getDetSummary(const uint32_t detId, std::vector<float>& performanceValues) const;
 
-   bool setNumberOfDigis(uint32_t detId, float mean, float RMS);
-   bool setNoisePercentage(uint32_t detId, float percentage);
+// RawData 
+  bool setNumberOfRawDataErrors(uint32_t detId, float mean, float rms); 
+  bool setRawDataErrorType(uint32_t detId, int bin, float percentage);
+  bool setTBMType(uint32_t detId, int bin, float percentage);
+  bool setTBMMessage(uint32_t detId, int bin, float percentage);
+  bool setFEDfullType(uint32_t detId, int bin, float percentage); 
+  bool setFEDtimeoutChannel(uint32_t detId, int bin, float percentage); 
+  bool setSLinkErrSize(uint32_t detId, float mean, float rms); 
+  bool setFEDmaxErrLink(uint32_t detId, float maxErrID); 
+  bool setmaxErr36ROC(uint32_t detId, float maxErrID); 
+  bool setmaxErrDCol(uint32_t detId, float maxErrID); 
+  bool setmaxErrPixelRow(uint32_t detId, float maxErrID); 
+  bool setmaxErr38ROC(uint32_t detId, float maxErrID); 
+// Digi
+  bool setNumberOfDigis(uint32_t detId, float mean, float rms);
+  bool setADC(uint32_t detId, float mean, float rms);
+  bool setDigimapHotCold(uint32_t detId, float hot, float cold);
+// Cluster
+  bool setNumberOfClusters(uint32_t detId, float mean, float rms);
+  bool setClusterCharge(uint32_t detId, float mean, float rms); 
+  bool setClusterSizeX(uint32_t detId, float mean, float rms);
+  bool setClusterSizeY(uint32_t detId, float mean, float rms);
+  bool setClustermapHotCold(uint32_t detId, float hot, float cold);
+// RecHit
+  bool setNumberOfRecHits(uint32_t detId, float mean, float rms);
+  bool setRecHitMatchedClusterSizeX(uint32_t detId, float mean, float rms);
+  bool setRecHitMatchedClusterSizeY(uint32_t detId, float mean, float rms);
+  bool setRecHitmapHotCold(uint32_t detId, float hot, float cold);
+// TrackResidual: 
+  bool setResidualX(uint32_t detId, float mean, float rms);
+  bool setResidualY(uint32_t detId, float mean, float rms);
 
 private:
    bool setValue(uint32_t detid, int index, float performanceValue);
@@ -69,6 +114,7 @@ private:
 private: 
   std::vector<DetSummary> allDetSummaries_;
   unsigned int runNumber_;
+  unsigned int numberOfEvents_;
   unsigned long long timeValue_;
 };
 
