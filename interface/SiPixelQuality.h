867d3ffe125fefa2b549f0163689e0509a133ed6
heyburn <sha1-e4a423ee800007234c87315cd94059f9121f1ef8@cern.ch>
2008-10-16 13:51:02 +0000

    added new methods for FED/ROC location

--- interface/SiPixelQuality.h
+++ interface/SiPixelQuality.h
@@ -22,6 +22,26 @@ class SiPixelQuality {
     unsigned short BadRocs;
   };
 
+      //////////////////////////////////////
+      //  errortype "whole" = int 0 in DB //
+      //  errortype "tbmA" = int 1 in DB  //
+      //  errortype "tbmB" = int 2 in DB  //
+      //  errortype "none" = int 3 in DB  //
+      //////////////////////////////////////
+
+      /////////////////////////////////////////////////
+      //each bad roc correspond to a bit to 1: num=  //
+      // 0 <-> all good rocs                         //
+      // 1 <-> only roc 0 bad                        //
+      // 2<-> only roc 1 bad                         //
+      // 3<->  roc 0 and 1 bad                       //
+      // 4 <-> only roc 2 bad                        //
+      //  ...                                        //
+      /////////////////////////////////////////////////
+
+
+
+
 
   class BadComponentStrictWeakOrdering{
   public:
@@ -51,7 +71,7 @@ class SiPixelQuality {
   // remove disabled module from the list
   // returns false if id not in disable list, true otherwise
   //  bool removeDisabledModule(const disabledModuleType & module);
-  bool removeDisabledModule(const uint32_t & detid);
+  //   bool removeDisabledModule(const uint32_t & detid); 
 
 
 //--------------- Interface for the user -----------------//
@@ -63,22 +83,25 @@ class SiPixelQuality {
   bool IsModuleUsable(const uint32_t& detid) const;  //returns True if module NOT disabled
   bool IsRocBad(const uint32_t& detid, const short& rocNb) const;  //returns True if ROC is disabled
   short getBadRocs(const uint32_t& detid) const;  //returns bad Rocs for given DetId
+  //each bad roc correspond to a bit to 1: num=
+  //0 <-> all good rocs
+  //1 <-> only roc 0 bad
+  //2<-> only roc 1 bad
+  //3<->  roc 0 and 1 bad
+  // 4 <-> only roc 2 bad
+  //...
   const std::vector<disabledModuleType> getBadComponentList() const  //returns list of disabled modules/ROCs
     { return theDisabledModules; }
+  
+  const std::vector< std::pair <uint8_t, uint8_t> > getBadRocPositions(const uint32_t & detid) const; 
+
 
 
  private:
   std::vector<disabledModuleType> theDisabledModules;
+  bool IsFedBad(const uint32_t & detid) const; 
 
 }; // class SiPixelQuality
-
-
-
-
-
-
-
-
 
 
 #endif
--- src/SiPixelQuality.cc
+++ src/SiPixelQuality.cc
@@ -9,6 +9,25 @@
 
 #include <algorithm>
 
+
+      //////////////////////////////////////
+      //  errortype "whole" = int 0 in DB //
+      //  errortype "tbmA" = int 1 in DB  //
+      //  errortype "tbmB" = int 2 in DB  //
+      //  errortype "none" = int 3 in DB  //
+      //////////////////////////////////////
+    
+      /////////////////////////////////////////////////
+      //each bad roc correspond to a bit to 1: num=  //
+      // 0 <-> all good rocs                         //
+      // 1 <-> only roc 0 bad                        //
+      // 2<-> only roc 1 bad                         //
+      // 3<->  roc 0 and 1 bad                       //
+      // 4 <-> only roc 2 bad                        //
+      //  ...                                        //
+      /////////////////////////////////////////////////
+
+
 // add a list of modules to the vector of disabled modules
 void SiPixelQuality::addDisabledModule(std::vector<SiPixelQuality::disabledModuleType> & idVector) {
   theDisabledModules.insert(theDisabledModules.end(),
@@ -17,67 +36,65 @@ void SiPixelQuality::addDisabledModule(std::vector<SiPixelQuality::disabledModul
 
 } 
 
-// void SiPixelQuality::addDisabledModule(std::vector<SiPixelQuality::disabledModuleType> idVector)
-// remove disabled module from the list
-// returns false if id not in disable list, true otherwise
 
+// bool SiPixelQuality::removeDisabledModule(const uint32_t & detid){
+//   std::vector<disabledModuleType>::const_iterator iter = std::lower_bound(theDisabledModules.begin(),theDisabledModules.end(),detid,SiPixelQuality::BadComponentStrictWeakOrdering());
+//   if (iter != theDisabledModules.end() && iter->DetID==detid){
+//     theDisabledModules.erase(iter);
+//     return true;
+//   }
+//   return false;
+// }
 
-// bool SiPixelQuality::removeDisabledModule(const SiPixelQuality::disabledModuleType &module) {
-//   std::vector<SiPixelQuality::disabledModuleType>::iterator iter = find(
-// 										theDisabledModules.begin(),
-// 										theDisabledModules.end(),
-// 										module
-// 										);
-//   if (iter == theDisabledModules.end())
-//     return false;
-  
-//   theDisabledModules.erase(iter);
-//   return true;
-
-// } 
-
-bool SiPixelQuality::removeDisabledModule(const uint32_t & detid){
-
-  return true;
-}
-// bool SiPixelQuality::removeDisabledModule(disabledModuleType module)
-// method to return true if the specified module is in the list
-// of disabled modules
-
-//bool SiPixelQuality::isModuleDisabled(SiPixelQuality::disabledModuleType module) {
-//  std::vector<SiPixelQuality::disabledModuleType>::const_iterator iter = find(theDisabledModules.begin(),
-//						     theDisabledModules.end(),
-//						     module);
-//  return iter != theDisabledModules.end();
-
-
-
-
+// ask if module is OK
 bool SiPixelQuality::IsModuleUsable(const uint32_t& detid) const {
   std::vector<disabledModuleType>::const_iterator iter = std::lower_bound(theDisabledModules.begin(),theDisabledModules.end(),detid,SiPixelQuality::BadComponentStrictWeakOrdering());
-  if (iter!=theDisabledModules.end() && iter->DetID==detid)
+  if (iter != theDisabledModules.end() && iter->DetID==detid)
       return false;
   return true;
 }
 
-
+//ask if module is bad
  bool SiPixelQuality::IsModuleBad(const uint32_t & detid) const {
+   if(IsFedBad(detid))
+     return true;
   std::vector<disabledModuleType>::const_iterator iter = std::lower_bound(theDisabledModules.begin(),theDisabledModules.end(),detid,SiPixelQuality::BadComponentStrictWeakOrdering());
-  if (iter!=theDisabledModules.end() && iter->DetID==detid)
-    //    return iter->BadModule;
+  if (iter!=theDisabledModules.end() && iter->DetID==detid && iter->errorType == 0)  //errorType 0 corresponds to "whole" dead module
      return true;
   return false;
 }
 
-
-
+//ask if roc is bad
 bool SiPixelQuality::IsRocBad(const uint32_t& detid, const short& rocNb) const {
-    return true; 
-  }
+  if(IsModuleBad(detid))
+    return true;
+  std::vector<disabledModuleType>::const_iterator iter = std::lower_bound(theDisabledModules.begin(),theDisabledModules.end(),detid,SiPixelQuality::BadComponentStrictWeakOrdering());
+  if (iter != theDisabledModules.end() && iter->DetID == detid)
+    return ((iter->BadRocs >> rocNb)&0x1);
+  return false;
+}
+
+//ask for bad ROCS
 
 
 short SiPixelQuality::getBadRocs(const uint32_t& detid) const{
-  return 1; 
+  std::vector<disabledModuleType>::const_iterator iter = std::lower_bound(theDisabledModules.begin(),theDisabledModules.end(),detid,SiPixelQuality::BadComponentStrictWeakOrdering());
+  if (iter != theDisabledModules.end() && iter->DetID==detid)
+    return iter->BadRocs;
+  return 0;
 }
+
+const std::vector< std::pair <uint8_t, uint8_t> > SiPixelQuality::getBadRocPositions(const uint32_t & detid) const{
+  std::vector< std::pair <uint8_t, uint8_t> > badrocpositions (0);
+  std::pair<uint8_t, uint8_t> coord(1,1);
+  //  badrocpositions.push_back(coord);
+  return badrocpositions;
+}
+
+
+bool SiPixelQuality::IsFedBad(const uint32_t & detid) const{
+  return false;
+}
+
 
 
